fc4e835cd971517942da301957ea07161f0ef696
Kiri-1 <69604321+Kiri-1@users.noreply.github.com>
2022-10-18 14:35:55 -0300

    make it better

--- BackEnd/src/main/java/com/portfolio/demo/Security/Controller/AuthController.java
+++ BackEnd/src/main/java/com/portfolio/demo/Security/Controller/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origin="")
+@CrossOrigin
 public class AuthController {
 
     @Autowired
